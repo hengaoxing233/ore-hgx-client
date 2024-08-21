babc6559c4ba0a550b165c65666a6c941761e926
hengaoxing233 <76994928+hengaoxing233@users.noreply.github.com>
2024-08-21 10:28:34 +0800

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use clap::{Parser, Subcommand};
+use mine::MineArgs;
+
+mod mine;
+
+// --------------------------------
+
+/// A command line interface tool for pooling power to submit hashes for proportional ORE rewards
+#[derive(Parser, Debug)]
+#[command(version, author, about, long_about = None)]
+struct Args {
+    #[arg(long,
+        value_name = "SERVER_URL",
+        help = "URL of the server to connect to",
+        default_value = "domainexpansion.tech",
+    )]
+    url: String,
+    #[command(subcommand)]
+    command: Commands
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[command(about = "Connect to pool and start mining.")]
+    Mine(MineArgs),
+}
+
+// --------------------------------
+
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let base_url = args.url;
+    match args.command {
+        Commands::Mine(args) => {
+            mine::mine(args, base_url).await;
+        },
+    }
+
+
+}
+
---
+++ src/mine.rs
@@ -0,0 +1,242 @@
+use std::{ops::Range, sync::Arc, time::{Duration, Instant}};
+use std::sync::Mutex;
+use clap::{arg, Parser};
+use drillx_2::equix;
+use futures_util::SinkExt;
+use base64::prelude::*;
+use serde_json::Value;
+use core_affinity;
+
+#[derive(Debug)]
+pub enum ServerMessage {
+    StartMining([u8; 32], Range<u64>, u64)
+}
+
+#[derive(Debug, Parser)]
+pub struct MineArgs {
+    #[arg(
+        long,
+        value_name = "CORES_COUNT",
+        help = "The number of CPU cores to allocate to mining",
+        global = true
+    )]
+    cores: Option<usize>,
+}
+
+fn create_shared_client() -> Arc<reqwest::blocking::Client> {
+    Arc::new(
+        reqwest::blocking::Client::builder()
+            .http1_title_case_headers()
+            .danger_accept_invalid_certs(true)
+            .connection_verbose(true)
+            .build()
+            .unwrap(),
+    )
+}
+
+fn array_to_base64(data: &[u8; 32]) -> String {
+    base64::encode(data)
+}
+
+fn base64_to_array(base64_string: &str) -> Result<[u8; 32], &'static str> {
+    let decoded_bytes = base64::decode(base64_string).map_err(|_| "Invalid Base64 input")?;
+
+    let array: [u8; 32] = decoded_bytes.as_slice().try_into().map_err(|_| "Decoded byte length is not 32")?;
+
+    Ok(array)
+}
+
+fn u8_16_to_base64(data: [u8; 16]) -> String {
+    base64::encode(&data)
+}
+
+fn base64_to_u8_16(encoded: &str) -> Result<[u8; 16], base64::DecodeError> {
+    let decoded = base64::decode(encoded)?;
+    let mut array = [0u8; 16];
+    array.copy_from_slice(&decoded);
+    Ok(array)
+}
+
+fn u8_8_to_base64(data: [u8; 8]) -> String {
+    base64::encode(&data)
+}
+
+fn base64_to_u8_8(encoded: &str) -> Result<[u8; 8], base64::DecodeError> {
+    let decoded = base64::decode(encoded)?;
+    let mut array = [0u8; 8];
+    array.copy_from_slice(&decoded);
+    Ok(array)
+}
+
+pub async fn mine(args: MineArgs, url: String) {
+    let client = create_shared_client();
+    let server_url = url.clone();
+    println!("服务端url:{}", server_url);
+    let cores = args.cores.unwrap_or_else(|| num_cpus::get());
+
+    let mut challenge_str = String::new();
+    let mut server_pubkey = String::new();
+    let mut min_difficulty = 0;
+    let mut nonce_start = 0;
+    let mut nonce_end = 0;
+
+    loop {
+        println!("获取任务---------------------------------------------");
+        let res = client.get(format!("{server_url}/getchallenge"))
+            .header("Content-Type", "application/json")
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0")
+            .send();
+        match res {
+            Ok(response) => match response.text() {
+                Ok(text) => {
+                    println!("获取任务响应内容: {}", text);
+                    if let Ok(json) = serde_json::from_str::<Value>(&text) {
+                        if let Some(ok) = json["code"].as_u64() {
+                            if ok == 1 {
+                                challenge_str = json["challenge"].as_str().unwrap_or_default().to_string();
+                                server_pubkey = json["pubkey"].as_str().unwrap_or_default().to_string();
+                                min_difficulty = json["min_difficulty"].as_u64().unwrap_or_default() as u32;
+                                nonce_start = json["nonce_start"].as_u64().unwrap_or_default();
+                                nonce_end = json["nonce_end"].as_u64().unwrap_or_default();
+                            } else {
+                                println!("获取任务失败");
+                                tokio::time::sleep(Duration::from_secs(1)).await;
+                                continue;
+                            }
+                        }
+                    }
+                },
+                Err(e) => {
+                    println!("获取任务失败: {:?}", e);
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                    continue;
+                }
+            },
+            Err(e) => {
+                println!("获取任务失败: {:?}", e);
+                tokio::time::sleep(Duration::from_secs(1)).await;
+                continue;
+            }
+        }
+
+        println!("已接收到挖矿消息!");
+        println!("开始挖矿...");
+        println!("Nonce range: {} - {}", nonce_start, nonce_end);
+        println!("challenge: {}", challenge_str);
+        let mut challenge = [0;32];
+        match base64_to_array(&challenge_str) {
+            Ok(ret) => challenge = ret,
+            Err(_) => {
+                continue
+            }
+        }
+        let challenge = base64_to_array(&challenge_str).unwrap();
+        let hash_timer = Instant::now();
+
+        let core_ids = Arc::new(core_affinity::get_core_ids().unwrap());
+
+        let total_nonce = nonce_end - nonce_start; // 总的nonce范围
+        let step = total_nonce / cores as u64; // 每个核心需要处理的nonce数量
+        let remainder = total_nonce % cores as u64; // 处理不能被均分的nonce部分
+
+        let handles = (0..cores).map(|i| {
+            let challenge = challenge.clone(); // 确保 challenge 被正确克隆到每个线程中
+            let core_ids = Arc::clone(&core_ids); // 克隆 Arc 以避免移动
+            std::thread::spawn(move || {
+                let mut memory = equix::SolverMemory::new();
+                if let Some(core_id) = core_ids.get(i) {
+                    core_affinity::set_for_current(*core_id);
+                }
+                // 为每个核心分配 nonce 范围
+                let start_nonce = nonce_start + i as u64 * step + if i < remainder as usize { i as u64 } else { remainder };
+                let end_nonce = start_nonce + step + if i < remainder as usize { 1 } else { 0 }; // 保证范围覆盖
+
+                let mut best_nonce = start_nonce;
+                let mut best_difficulty = 0;
+                let mut best_hash = drillx_2::Hash::default();
+                let mut total_hashes: u64 = 0;
+
+                let hash_timer = Instant::now(); // 计时器开始
+                let mut nonce = start_nonce;
+
+                // 挖矿循环，直到达到nonce范围或超时
+                while nonce < end_nonce && hash_timer.elapsed().as_secs() < 10 {
+                    // Create hash
+                    for hx in  drillx_2::get_hashes_with_memory(&mut memory, &challenge, &nonce.to_le_bytes()) {
+                        total_hashes += 1;
+                        let difficulty = hx.difficulty();
+                        if difficulty.gt(&best_difficulty) {
+                            best_nonce = nonce;
+                            best_difficulty = difficulty;
+                            best_hash = hx;
+                        }
+                    }
+
+                    // Increment nonce
+                    nonce += 1;
+                }
+
+                Some((best_nonce, best_difficulty, best_hash, total_hashes))
+            })
+        }).collect::<Vec<_>>();
+
+        let mut best_nonce: u64 = 0;
+        let mut best_difficulty = 0;
+        let mut best_hash = drillx_2::Hash::default();
+        let mut total_nonces_checked = 0;
+        for h in handles {
+            if let Ok(Some((nonce, difficulty, hash, nonces_checked))) = h.join() {
+                total_nonces_checked += nonces_checked;
+                if difficulty > best_difficulty {
+                    best_difficulty = difficulty;
+                    best_nonce = nonce;
+                    best_hash = hash;
+                }
+            }
+        }
+
+        let hash_time = hash_timer.elapsed();
+        println!("找到最佳diff: {}", best_difficulty);
+        println!("Processed: {}", total_nonces_checked);
+        println!("Hash time: {:?}", hash_time);
+
+        if best_difficulty < min_difficulty {
+            println!("难度太低，丢弃，重新计算");
+            continue;
+        }
+
+        let res = client.post(format!("{server_url}/setsolution"))
+            .header("Accept", "application/json, text/plain, */*")
+            .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0")
+            .header("Content-Type", "application/json")
+            .json(&serde_json::json!({
+                "challenge": challenge_str,
+                "d": u8_16_to_base64(best_hash.d),
+                "n": u8_8_to_base64(best_nonce.to_le_bytes()),
+                "difficulty": best_difficulty,
+                "pubkey": server_pubkey,
+            }))
+            .send();
+
+        match res {
+            Ok(response) => match response.text() {
+                Ok(text) => {
+                    println!("提交哈希响应内容: {}", text);
+                    if let Ok(json) = serde_json::from_str::<Value>(&text) {
+                        if let Some(ok) = json["code"].as_u64() {
+                            if ok == 1 {
+                                println!("提交哈希成功");
+                            } else {
+                                println!("提交哈希失败");
+                            }
+                        }
+                    }
+                },
+                Err(e) => {
+                    println!("提交哈希失败: {:?}", e);
+                }
+            },
+            Err(e) => println!("提交哈希失败: {:?}", e),
+        }
+    }
+}
